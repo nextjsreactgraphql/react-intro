b548cdd114dd958358a7354edf275b5e7c87448b
Nils Hartmann <nils@nilshartmann.net>
2025-02-26 11:24:38 +0100

    02 : Properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import ArticleCard from "./ArticleCard.tsx";
+import { Fragment } from "react";
 
 export default function App() {
-  return <ArticleCard />;
+  return (
+    <>
+      <ArticleCard title={"Artikel 1"} likes={123} />
+      <ArticleCard title={"Artikel 2"} likes={45} />
+    </>
+  );
 }
--- src/ArticleCard.tsx
+++ src/ArticleCard.tsx
@@ -1,11 +1,32 @@
-export default function ArticleCard() {
+// Properties
+// <ArticleCard title="Hello React" likes={234} />
+// { title: "Hello React", likes: 234 }
+
+type ArticleCardProps = {
+	title: string;
+	likes: number
+}
+
+//export default function ArticleCard(props: ArticleCardProps) {
+export default function ArticleCard({title, likes} : ArticleCardProps) {
+	// const {title, likes} = props;
 	// return React.createElement("article")
 	// JSX
 	// "virtual DOM"
+
+	const myClassNames = "text-2xl font-bold text-amber-700"
+
+	if (title === "" ) {
+		return <h1>Artikel kaputt</h1>
+	}
+
+
+
 	return (
 		<article>
-			<h1 className="text-2xl font-bold text-amber-700">Hello React</h1>
-			<div>234 Likes</div>
+			<h1 id={"title"} aria-hidden={true} className={myClassNames}>{title}</h1>
+			{/*<div>{likes} Likes</div>*/}
+			{likes === 0 ? <p>Leider keine Likes!</p> : <p>{likes} Likes</p>}
 		</article>
 	)
 }
