b4d57f9720e675a59b1922915c4216abe8729c2e
Nils Hartmann <nils@nilshartmann.net>
2025-09-10 14:00:03 +0200

    04: Listen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import ArticleCard from "./ArticleCard.tsx";
 import { Fragment } from "react";
+import ArticleList from "./ArticleList.tsx";
 
+const article1 = {
+  id: "1",
+  likes: 421,
+  title: "Hello React",
+};
+
+const article2 = {
+  id: "2",
+  likes: 0,
+  title: "Hello Next.js",
+};
+function loadArticlesFromServer() {
+  return [article1, article2];
+}
 export default function App() {
+
+  // "Serverzugriff"
+  const allArticles = loadArticlesFromServer();
+
   return <Fragment>
-      <ArticleCard
-      title="Learning React"
-      likes={7}
-    />
-    <ArticleCard
-      title="Learning Next"
-      likes={12}
-    />
+    <ArticleList articles={allArticles} />
+    {/*  <ArticleCard*/}
+    {/*  title="Learning React"*/}
+    {/*  likes={7}*/}
+    {/*/>*/}
+    {/*<ArticleCard*/}
+    {/*  title="Learning Next"*/}
+    {/*  likes={12}*/}
+    {/*/>*/}
   </Fragment>;
 }
 
---
+++ src/ArticleList.tsx
@@ -0,0 +1,15 @@
+import { Article } from "./types.ts";
+import ArticleCard from "./ArticleCard.tsx";
+
+type ArticleListProps = {
+  articles: Article[]
+}
+
+export default function ArticleList(props: ArticleListProps) {
+
+  return <div className={"flex gap-x-4"}>
+    {props.articles.map(a =>
+      <ArticleCard key={a.id} title={a.title} likes={a.likes} />)}
+  </div>
+
+}
