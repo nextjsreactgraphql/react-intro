55c01dfec4990acefb0b7e4e8857c8109013760f
Nils Hartmann <nils@nilshartmann.net>
2025-09-29 10:19:42 +0200

    01: React Komponente

---
+++ src/ArticleCard.tsx
@@ -0,0 +1,16 @@
+export default function ArticleCard() {
+
+  // Model
+  // View <----
+  // Controller
+  // MVC-Pattern
+
+  // JSX "Templatesprache"
+  return <article>
+    <h1 className="text-2xl font-bold">Artikel eins</h1>
+    <div>1234 Likes</div>
+  </article>
+
+
+
+}
